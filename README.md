6bbfa496dc80f5ab56b0a6b1f823058f5c4abced
J Neal <jasmineneal101@gmail.com>
2023-04-16 20:05:44 +1000

    add write file function

---
+++ README.md
@@ -0,0 +1,40 @@
+# f
+  ![badge](https://img.shields.io/badge/license-agpl-blue)
+
+## Table of Contents:
+-[Description](#description)
+-[Licence](#licence)
+-[Installation](#installation)
+-[Usage](#usage)
+-[Website Link](#website-link)
+-[How to Contribute](#how-to-contribute)
+-[Tests](#tests)
+-[Questions](#questions)
+
+## Description
+f
+undefined
+
+## Installation  
+f
+
+## Usage  
+ff
+
+## Website Link
+Link to the deployed site: [Deployed Site](undefined)
+
+## License
+  This project is protected under the agpl license
+      
+
+## How to Contribute
+f
+
+## Tests
+Tests for the project:
+
+## Questions
+Please contact me for questions using the following links
+f
+https://github.com/f
--- question.js
+++ question.js
@@ -1,56 +1,57 @@
 const questions = [
-    {
-      type: 'input',
-      message: 'What is your project title?',
-      name: 'title',
-    },
-    {
-      type: 'input',
-      message: 'Please provide a description of your project',
-      name: 'Description',
-    },
-    {
-        type: 'input',
-        message: 'Please provide installation instructions for users',
-        name: 'Installation',
-      },
-      {
-        type: 'input',
-        message: 'Please provide examples for use',
-        name: 'Usage',
-      },
-      {
-        type: 'input',
-        message: 'Please enter the link to the deployed Github Pages',
-        name: 'Wesbite',
-      },
-      {
-        type: 'input',
-        message: 'Writes tests for your application and provide examples on how to run them',
-        name: 'Tests',
-      },
-      {
-        type: 'list',
-        name: 'license',
-        message: 'Please select what license will be used',
-        choices: ['agpl', 'apache', 'mit', 'none']
-    },
-    {
-        type: 'input',
-        message: 'Please enter how other developers can contribute to your project',
-        name: 'Contributing',
-      },
-      {
-        type: 'input',
-        message: 'Please enter your email address users can contact for questions',
-        name: 'Email',
-      },
-      {
-        type: 'input',
-        message: 'Please enter your github username so users can reach out with questions',
-        name: 'Username',
-      },
-
+  {
+    type: "input",
+    message: "What is your project title?",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "Please provide a description of your project",
+    name: "Description",
+  },
+  {
+    type: "input",
+    message: "Please provide installation instructions for users",
+    name: "Installation",
+  },
+  {
+    type: "input",
+    message: "Please provide examples for use",
+    name: "Usage",
+  },
+  {
+    type: "input",
+    message: "Please enter the link to the deployed Github Pages",
+    name: "Wesbite",
+  },
+  {
+    type: "input",
+    message:
+      "Writes tests for your application and provide examples on how to run them",
+    name: "Tests",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Please select what license will be used",
+    choices: ["agpl", "apache", "mit", "none"],
+  },
+  {
+    type: "input",
+    message: "Please enter how other developers can contribute to your project",
+    name: "Contributing",
+  },
+  {
+    type: "input",
+    message: "Please enter your email address users can contact for questions",
+    name: "Email",
+  },
+  {
+    type: "input",
+    message:
+      "Please enter your github username so users can reach out with questions",
+    name: "Username",
+  },
 ];
 
-module.exports=questions
+module.exports = questions;
